508355df5dfec70620fd7377c8d4631751f38e5f
Geoffrey Knox <geoffreyknox56@gmail.com>
2019-06-11 07:17:23 -0600

    walking magnitude changes

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -18,6 +18,26 @@ public class CharacterMovement : MonoBehaviour
     private GameObject rl;
 
 
+    private float RunForward = 2.661f;
+    private float WalkForward = 2.275f;
+    private float RunBackward = 1.68f;
+    private float WalkBackward = 2.234f;
+
+    private float RunStrafeRight = 1.818f;
+    private float RunStrafeLeft = 1.814f;
+    private float WalkStrafeLeft = 1.867f;
+    private float WalkStrafeRight = 1.583f;
+
+    private float RunForwardLeft = 2.659f;
+    private float RunForwardRight = 2.533f;
+    private float WalkForwardRight = 2.387f;
+    private float WalkForwardLeft = 2.355f;
+
+    private float RunBackwardLeft = 2.41f;
+    private float RunBackwardRight = 2.084f;
+    private float WalkBackwardRight = 2.356f;
+    private float WalkBackwardLeft = 2.335f;
+
     void Start()
     {
         shouldWalk = false;
@@ -30,14 +50,12 @@ public class CharacterMovement : MonoBehaviour
         fire_position = gameObject.transform.Find("player/fire_position");
         ll = GameObject.Find("player/SpaceMan@Rifle Aiming Idle/mixamorig:Hips/mixamorig:LeftUpLeg/mixamorig:LeftLeg/mixamorig:LeftFoot/mixamorig:LeftToeBase");
         rl = GameObject.Find("player/SpaceMan@Rifle Aiming Idle/mixamorig:Hips/mixamorig:RightUpLeg/mixamorig:RightLeg/mixamorig:RightFoot/mixamorig:RightToeBase");
+      
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            UnityEditor.EditorApplication.isPaused = true;
-        }
+        Debug.Log(anim.GetCurrentAnimatorStateInfo(0).IsName("Walk Backward Left"));
     }
 
     private void FixedUpdate()
@@ -74,9 +92,86 @@ public class CharacterMovement : MonoBehaviour
             anim.SetFloat("direction_y", 0);
             anim.SetFloat("direction_x", 0);
         }
-        Debug.Log("step distance = " + Vector3.Distance(ll.transform.position, rl.transform.position));
-        movement *= distance * speed;
+        //Debug.Log("step distance = " + Vector3.Distance(ll.transform.position, rl.transform.position));
+        //joystick_distance(float) * speed(float) * joystick_direction(vector)?
+        movement *= speed * distance;
+        //if(distance <= 0.5f)
+        //{
+        //    anim.speed = distance * 2f;
+        //}
+        Debug.Log("x1 = " + x1);
+        Debug.Log("y1 = " + y1);
         pc.SimpleMove(movement);
+    }
+
+    float GetWalkingMagnitude()
+    {
+        float outFloat = 0f;
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("Run Forward"))
+        {
+            outFloat = RunForward;
+        }
+        else if(anim.GetCurrentAnimatorStateInfo(0).IsName("Rifle Walk Forward"))
+        {
+            outFloat = WalkForward;
+        }
+        else if(anim.GetCurrentAnimatorStateInfo(0).IsName("Walk Backward"))
+        {
+            outFloat = WalkBackward;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run Backward"))
+        {
+            outFloat = RunBackward;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run Forward Left"))
+        {
+            outFloat = RunForwardLeft;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run Forward Right"))
+        {
+            outFloat = RunForwardRight;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Walk Forward Left"))
+        {
+            outFloat = WalkForwardLeft;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Walk Forward Right"))
+        {
+            outFloat = WalkForwardRight;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Walk Strafe Left"))
+        {
+            outFloat = WalkStrafeLeft;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Walk Strafe Right"))
+        {
+            outFloat = WalkStrafeRight;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run Strafe Right"))
+        {
+            outFloat = RunStrafeRight;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run Strafe Left"))
+        {
+            outFloat = RunStrafeLeft;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Walk Backward Left"))
+        {
+            outFloat = WalkBackwardLeft;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Walk Backward Right"))
+        {
+            outFloat = WalkBackwardRight;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run Backward Right"))
+        {
+            outFloat = RunBackwardRight;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run Backward Left"))
+        {
+            outFloat = RunBackwardLeft;
+        }
+        return outFloat;
     }
 
     public static Vector3 ClampMagnitude(Vector3 v, float max, float min)
