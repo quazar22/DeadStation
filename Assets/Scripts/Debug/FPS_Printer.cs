d819ba861e895313463b1856cadc1eea5a8db598
Geoffrey Knox <geoffreyknox56@gmail.com>
2019-11-28 16:24:48 -0700

    another save

---
+++ Assets/Scripts/Debug/FPS_Printer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS_Printer : MonoBehaviour
+{
+    float deltaTime = 0.0f;
+    float fps = 0.0f;
+
+    Text t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += Time.deltaTime;
+        deltaTime /= 2.0f;
+        fps = 1.0f / deltaTime;
+        t.text = fps.ToString();
+    }
+}
